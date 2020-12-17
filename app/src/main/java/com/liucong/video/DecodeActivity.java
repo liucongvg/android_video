84d3bfae99c87982d712e2951339e7ea76e39fb4
liucong02 <liucong02@corp.netease.com>
2020-12-17 10:52:53 +0800

    play audio

--- app/src/main/java/com/liucong/video/DecodeActivity.java
+++ app/src/main/java/com/liucong/video/DecodeActivity.java
@@ -5,12 +5,14 @@ import java.nio.ByteBuffer;
 
 import android.app.Activity;
 import android.graphics.Rect;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.media.MediaCodec;
 import android.media.MediaCodec.BufferInfo;
 import android.media.MediaExtractor;
 import android.media.MediaFormat;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.Log;
 import android.view.Surface;
 import android.view.SurfaceHolder;
@@ -18,7 +20,8 @@ import android.view.SurfaceView;
 
 public class DecodeActivity extends Activity implements SurfaceHolder.Callback {
     private static final String SAMPLE = "/sdcard" + "/video.mp4";
-    private PlayerThread mPlayer = null;
+    private VideoThread mVideoThread = null;
+    private AudioThread mAudioThread = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,30 +38,34 @@ public class DecodeActivity extends Activity implements SurfaceHolder.Callback {
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
         Rect rect = holder.getSurfaceFrame();
-        Log.d("liucong", "surface: "+(rect.right-rect.left)+" * "+(rect.top-rect.bottom));
+        Log.d("liucong", "surface: " + (rect.right - rect.left) + " * " + (rect.top - rect.bottom));
     }
 
     @Override
     public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-        if (mPlayer == null) {
-            mPlayer = new PlayerThread(holder.getSurface());
-            mPlayer.start();
+        if (mVideoThread == null) {
+            mVideoThread = new VideoThread(holder.getSurface());
+            mVideoThread.start();
+        }
+        if (mAudioThread == null) {
+            mAudioThread = new AudioThread();
+            mAudioThread.start();
         }
     }
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
-        if (mPlayer != null) {
-            mPlayer.interrupt();
+        if (mVideoThread != null) {
+            mVideoThread.interrupt();
         }
     }
 
-    private class PlayerThread extends Thread {
+    private class VideoThread extends Thread {
         private MediaExtractor extractor;
-        private MediaCodec decoder;
+        private MediaCodec videoDecoder;
         private Surface surface;
 
-        public PlayerThread(Surface surface) {
+        public VideoThread(Surface surface) {
             this.surface = surface;
         }
 
@@ -77,34 +84,34 @@ public class DecodeActivity extends Activity implements SurfaceHolder.Callback {
                 if (mime.startsWith("video/")) {
                     extractor.selectTrack(i);
                     try {
-                        decoder = MediaCodec.createDecoderByType(mime);
+                        videoDecoder = MediaCodec.createDecoderByType(mime);
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
                     int with = format.getInteger(MediaFormat.KEY_WIDTH);
                     int height = format.getInteger(MediaFormat.KEY_HEIGHT);
-                    Log.d("liucong", with+" * "+height);
-                    decoder.configure(format, surface, null, 0);
+                    Log.d("liucong", with + " * " + height);
+                    videoDecoder.configure(format, surface, null, 0);
                     break;
                 }
             }
 
-            if (decoder == null) {
+            if (videoDecoder == null) {
                 Log.e("DecodeActivity", "Can't find video info!");
                 return;
             }
 
-            decoder.start();
+            videoDecoder.start();
 
-            ByteBuffer[] inputBuffers = decoder.getInputBuffers();
-            ByteBuffer[] outputBuffers = decoder.getOutputBuffers();
+            ByteBuffer[] inputBuffers = videoDecoder.getInputBuffers();
+            ByteBuffer[] outputBuffers = videoDecoder.getOutputBuffers();
             BufferInfo info = new BufferInfo();
             boolean isEOS = false;
             long startMs = System.currentTimeMillis();
 
             while (!Thread.interrupted()) {
                 if (!isEOS) {
-                    int inIndex = decoder.dequeueInputBuffer(10000);
+                    int inIndex = videoDecoder.dequeueInputBuffer(10000);
                     if (inIndex >= 0) {
                         ByteBuffer buffer = inputBuffers[inIndex];
                         int sampleSize = extractor.readSampleData(buffer, 0);
@@ -113,34 +120,36 @@ public class DecodeActivity extends Activity implements SurfaceHolder.Callback {
                             // flag to decoder, we will get it again from the
                             // dequeueOutputBuffer
                             Log.d("DecodeActivity", "InputBuffer BUFFER_FLAG_END_OF_STREAM");
-                            decoder.queueInputBuffer(inIndex, 0, 0, 0, MediaCodec.BUFFER_FLAG_END_OF_STREAM);
+                            videoDecoder.queueInputBuffer(inIndex, 0, 0, 0, MediaCodec.BUFFER_FLAG_END_OF_STREAM);
                             isEOS = true;
                         } else {
-                            decoder.queueInputBuffer(inIndex, 0, sampleSize, extractor.getSampleTime(), 0);
+                            Log.d("DecodeActivity", "InputBuffer queueInputBuffer");
+                            videoDecoder.queueInputBuffer(inIndex, 0, sampleSize, extractor.getSampleTime(), 0);
                             extractor.advance();
                         }
                     }
                 }
 
-                int outIndex = decoder.dequeueOutputBuffer(info, 10000);
+                int outIndex = videoDecoder.dequeueOutputBuffer(info, 10000);
                 switch (outIndex) {
                     case MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED:
                         Log.d("DecodeActivity", "INFO_OUTPUT_BUFFERS_CHANGED");
-                        outputBuffers = decoder.getOutputBuffers();
+                        outputBuffers = videoDecoder.getOutputBuffers();
                         break;
                     case MediaCodec.INFO_OUTPUT_FORMAT_CHANGED:
-                        Log.d("DecodeActivity", "New format " + decoder.getOutputFormat());
+                        Log.d("DecodeActivity", "New format " + videoDecoder.getOutputFormat());
                         break;
                     case MediaCodec.INFO_TRY_AGAIN_LATER:
                         Log.d("DecodeActivity", "dequeueOutputBuffer timed out!");
                         break;
                     default:
                         ByteBuffer buffer = outputBuffers[outIndex];
-                        Log.v("DecodeActivity", "We can't use this buffer but render it due to the API limit, " + buffer);
+                        //Log.v("DecodeActivity", "We can't use this buffer but render it due to the API limit, " + buffer);
 
                         // We use a very simple clock to keep the video FPS, or the video
                         // playback will be too fast
                         while (info.presentationTimeUs / 1000 > System.currentTimeMillis() - startMs) {
+                            //if(true)break;
                             try {
                                 sleep(10);
                             } catch (InterruptedException e) {
@@ -148,7 +157,7 @@ public class DecodeActivity extends Activity implements SurfaceHolder.Callback {
                                 break;
                             }
                         }
-                        decoder.releaseOutputBuffer(outIndex, true);
+                        videoDecoder.releaseOutputBuffer(outIndex, true);
                         break;
                 }
 
@@ -159,8 +168,115 @@ public class DecodeActivity extends Activity implements SurfaceHolder.Callback {
                 }
             }
 
-            decoder.stop();
-            decoder.release();
+            videoDecoder.stop();
+            videoDecoder.release();
+            extractor.release();
+        }
+    }
+
+    private class AudioThread extends Thread {
+        private MediaExtractor extractor;
+        private MediaCodec audioDecoder;
+        private int mSampleRate;
+        private int mChanel;
+        private long mDuration;
+
+        @Override
+        public void run() {
+            extractor = new MediaExtractor();
+            try {
+                extractor.setDataSource(SAMPLE);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            for (int i = 0; i < extractor.getTrackCount(); i++) {
+                MediaFormat format = extractor.getTrackFormat(i);
+                String mime = format.getString(MediaFormat.KEY_MIME);
+                if (mime.startsWith("audio/")) {
+                    extractor.selectTrack(i);
+                    mSampleRate = format.getInteger(MediaFormat.KEY_SAMPLE_RATE);
+                    mChanel = format.getInteger(MediaFormat.KEY_CHANNEL_COUNT);
+                    mDuration = format.getLong(MediaFormat.KEY_DURATION);
+                    try {
+                        audioDecoder = MediaCodec.createDecoderByType(mime);
+                        audioDecoder.configure(format, null, null, 0);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            if (audioDecoder == null) {
+                Log.e("DecodeActivity", "Can't find audio info!");
+                return;
+            }
+
+            audioDecoder.start();
+
+            ByteBuffer[] inputBuffers = audioDecoder.getInputBuffers();
+            ByteBuffer[] outputBuffers = audioDecoder.getOutputBuffers();
+            BufferInfo info = new BufferInfo();
+            boolean isEOS = false;
+            int buffsize = AudioTrack.getMinBufferSize(mSampleRate, AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT);
+            AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC, mSampleRate,
+                    AudioFormat.CHANNEL_OUT_STEREO,
+                    AudioFormat.ENCODING_PCM_16BIT,
+                    buffsize,
+                    AudioTrack.MODE_STREAM);
+            audioTrack.play();
+            while (!Thread.interrupted()) {
+                if (!isEOS) {
+                    int inIndex = audioDecoder.dequeueInputBuffer(10000);
+                    if (inIndex >= 0) {
+                        ByteBuffer buffer = inputBuffers[inIndex];
+                        int sampleSize = extractor.readSampleData(buffer, 0);
+                        if (sampleSize < 0) {
+                            // We shouldn't stop the playback at this point, just pass the EOS
+                            // flag to decoder, we will get it again from the
+                            // dequeueOutputBuffer
+                            Log.d("DecodeActivity", "InputBuffer BUFFER_FLAG_END_OF_STREAM");
+                            audioDecoder.queueInputBuffer(inIndex, 0, 0, 0, MediaCodec.BUFFER_FLAG_END_OF_STREAM);
+                            isEOS = true;
+                        } else {
+                            Log.d("DecodeActivity", "InputBuffer queueInputBuffer");
+                            audioDecoder.queueInputBuffer(inIndex, 0, sampleSize, extractor.getSampleTime(), 0);
+                            extractor.advance();
+                        }
+                    }
+                }
+
+                int outIndex = audioDecoder.dequeueOutputBuffer(info, 10000);
+                switch (outIndex) {
+                    case MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED:
+                        Log.d("DecodeActivity", "INFO_OUTPUT_BUFFERS_CHANGED");
+                        outputBuffers = audioDecoder.getOutputBuffers();
+                        break;
+                    case MediaCodec.INFO_OUTPUT_FORMAT_CHANGED:
+                        Log.d("DecodeActivity", "New format " + audioDecoder.getOutputFormat());
+                        break;
+                    case MediaCodec.INFO_TRY_AGAIN_LATER:
+                        Log.d("DecodeActivity", "dequeueOutputBuffer timed out!");
+                        break;
+                    default:
+                        ByteBuffer buffer = outputBuffers[outIndex];
+                        final byte[] chunk = new byte[info.size];
+                        buffer.get(chunk);
+                        buffer.clear();
+                        audioTrack.write(chunk, info.offset, info.offset + info.size);
+                        audioDecoder.releaseOutputBuffer(outIndex, false);
+                        break;
+                }
+
+                // All decoded frames have been rendered, we can stop playing now
+                if ((info.flags & MediaCodec.BUFFER_FLAG_END_OF_STREAM) != 0) {
+                    Log.d("DecodeActivity", "OutputBuffer BUFFER_FLAG_END_OF_STREAM");
+                    break;
+                }
+            }
+
+            audioDecoder.stop();
+            audioDecoder.release();
             extractor.release();
         }
     }
